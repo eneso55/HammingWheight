11320d0644e37a5402b0e4be7012fcc606a33a38
Enes <enesozturkk10@gmail.com>
2023-06-13 11:26:16 +0200

    Initial commit

---
+++ src/main/java/com/example/hammingwheight/HammingWheightApplication.java
@@ -0,0 +1,13 @@
+package com.example.hammingwheight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammingWheightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HammingWheightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hammingwheight/HammingWheightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hammingwheight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HammingWheightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
